fcb0c9b7c1e282006c0cfe9e01eaffeffabac931
lahbabic <lahbabic@gmail.com>
2021-12-02 20:02:33 +0100

    change variable name

--- c_hack.c
+++ c_hack.c
@@ -2,11 +2,11 @@
 
 int main(int argc, char const *argv[])
 {
-	//int limit_of_int = 4294967296;
-	int limit_of_int = 2147483647;
+	//int limit_of_int = 4294967295;
+	int swap_from_positif_to_negatif = 2147483647;
 	int add = 1;
-	printf("last int = %d\n", limit_of_int);
-	int after_limit = limit_of_int +add;
+	printf("last int = %d\n", swap_from_positif_to_negatif);
+	int after_limit = swap_from_positif_to_negatif +add;
 	printf("after haffa = %d\n", after_limit);
 	return 0;
 }
