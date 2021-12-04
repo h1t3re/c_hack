0749924b88ed54802840770df97a508619976d56
lahbabic <lahbabic@gmail.com>
2021-12-04 19:42:23 +0100

    Most significant bit pass from 0 to -1, 32 bits. Don't know why!

--- c_hack.c
+++ c_hack.c
@@ -19,7 +19,7 @@ int *get_ints(int number, int bitswanted)
 
 int main(int argc, char const *argv[])
 {
-	int number = 1;
+	int number = -1;
 	int bitswanted = 32;
 	int *bits = get_ints(number, bitswanted);
 	printf("%d = ", number);
@@ -30,7 +30,7 @@ int main(int argc, char const *argv[])
 	free(bits);
 	printf("\n");
 
-	number = 0;
+	number = 2147483647;
 	bits = get_ints(number, bitswanted);
 	printf("%d = ", number);
 	for (int i = bitswanted-1; i >= 0; --i)
@@ -40,7 +40,7 @@ int main(int argc, char const *argv[])
 	free(bits);
 	printf("\n");
 
-	number = -1;
+	number = 2147483648;
 	bits = get_ints(number, bitswanted);
 	printf("%d = ", number);
 	for (int i = bitswanted-1; i >= 0; --i)
