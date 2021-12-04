1c51e63c608f784d707220ebad0d7a02891db364
lahbabic <lahbabic@gmail.com>
2021-12-04 22:36:22 +0100

    1 bit is 32 bits with -1.

--- c_hack.c
+++ c_hack.c
@@ -9,17 +9,18 @@ int *get_ints(int number, int bitswanted)
 	int *bits = (int*)malloc(bitswanted*sizeof(int));
 	for (int i = 0; i < bitswanted; ++i)
 	{
-		mask = 1 << i;
+		mask = -1 << i;// c'est 32 bit mais dans chaque bit, il y'a 32 bits
 		mask_number = number & mask;
 		poid = mask_number >> i;
 		bits[i] = poid; 	
 	}	
+	printf("\n\n\n");
 	return bits;
 }
 
 int main(int argc, char const *argv[])
 {
-	int number = -1;
+	int number = 8;
 	int bitswanted = 32;
 	int *bits = get_ints(number, bitswanted);
 	printf("%d = ", number);
