e1d235eb4078bb1baf9d1455b2fe59cdd1f708aa
lahbabic <lahbabic@gmail.com>
2021-12-04 23:48:54 +0100

    from 0 to -1. Don't know why!

--- c_hack.c
+++ c_hack.c
@@ -9,18 +9,17 @@ int *get_ints(int number, int bitswanted)
 	int *bits = (int*)malloc(bitswanted*sizeof(int));
 	for (int i = 0; i < bitswanted; ++i)
 	{
-		mask = 1 << i;// c'est 32 bit mais dans chaque bit, il y'a 32 bits
+		mask = 1 << i;
 		mask_number = number & mask;
 		poid = mask_number >> i;
 		bits[i] = poid; 	
 	}	
-	printf("\n\n\n");
 	return bits;
 }
 
 int main(int argc, char const *argv[])
 {
-	int number = 1;
+	int number = 8;
 	int bitswanted = 32;
 	int *bits = get_ints(number, bitswanted);
 	printf("%d = ", number);
@@ -31,7 +30,7 @@ int main(int argc, char const *argv[])
 	free(bits);
 	printf("\n");
 
-	number = 0;
+	number = 2147483647;
 	bits = get_ints(number, bitswanted);
 	printf("%d = ", number);
 	for (int i = bitswanted-1; i >= 0; --i)
@@ -41,7 +40,7 @@ int main(int argc, char const *argv[])
 	free(bits);
 	printf("\n");
 
-	number = -1;
+	number = 2147483648;
 	bits = get_ints(number, bitswanted);
 	printf("%d = ", number);
 	for (int i = bitswanted-1; i >= 0; --i)
@@ -49,7 +48,7 @@ int main(int argc, char const *argv[])
 		printf("%d ", bits[i]);
 	}
 	free(bits);
-	printf("\n\n");
+	printf("\n");
 	
 	return 0;
 }
