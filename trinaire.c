703fae9df01239e4c106c682d0bad29d4d1ef0f5
lahbabic <lahbabic@gmail.com>
2021-12-29 15:46:17 +0100

    Final version.

--- trinaire.c
+++ trinaire.c
@@ -3,59 +3,202 @@
 #include <unistd.h>
 
 
-void convert_int_to_binary(int *bits, int number, int binary_length)
+void convert_int_to_binary(int *bits, int number, int mask, int binary_length)
 {
-	int mask = 0;
+	int mask_shifted = 0;
 	int number_and_mask = 0;
 	for (int i = 0; i < binary_length; ++i)
 	{
-		mask = 1 << i;
-		number_and_mask = number & mask;
+		mask_shifted = mask << i;
+		number_and_mask = number & mask_shifted;
 		bits[i] = number_and_mask >> i;
 	}
+	mask_shifted = 0;
+	number_and_mask = 0;
 }
+
 int main(int argc, char const *argv[])
 {
-	int int_length = 32;
-	int array_length = 7;
-	int *array = (int*)malloc(array_length*sizeof(int));
-	array[0] = 0;
-	array[1] = 1;
-	array[2] = 2147483647;
-	array[3] = 2147483648;
-	array[4] = 4294967295;
-	array[5] = -1;
-	array[6] = 4294967296;
-	int *bits = (int *)malloc(int_length*sizeof(int));
-	int *result = (int *)malloc(int_length*array_length*sizeof(int));
-	for (int j = 0; j < array_length; ++j)
+	int architecture = 32;
+	int *bits = (int *)malloc(architecture*sizeof(int));
+	int *bits0 = (int *)malloc(64*sizeof(int));
+	convert_int_to_binary(bits, 0, 1, architecture);
+	printf("mask 1 : 0 = %d = ", 0);
+	for (int i = architecture-1; i >= 0; --i)
 	{
-		if(j == 0){
-			printf("0 = ");
-		}else if(j == 1){
-			printf("1 = ");
-		}else if(j == 2){
-			printf("2147483647 = ");
-		}else if(j == 3){
-			printf("2147483648 = ");
-		}else if(j == 4){
-			printf("4294967295 = ");
-		}else if(j == 5){
-			printf("-1 = ");
-		}else if(j == 6){
-			printf("4294967296 = ");
-		}
-		printf("%d = ", array[j]);
-		convert_int_to_binary(bits, array[j], int_length);
-		for (int i = int_length-1; i >=	 0; --i)
-		{
-			result[32*j+(32-i-1)] = bits[i];
-			printf("%d ", result[32*j+(32-i-1)]);
-		}
-		printf("\n");
+		printf("%d ", bits[i]);
 	}
-	free(array);
+	printf("\n\n");
+	convert_int_to_binary(bits, 0, -1, architecture);
+	printf("mask -1 : 0 = %d = ", 0);
+	for (int i = architecture-1; i >= 0; --i)
+	{
+		printf("%d ", bits[i]);
+	}
+	printf("\n\n");
+	convert_int_to_binary(bits, 1, 1, architecture);
+	printf("mask 1 : 1 = %d = ", 1);
+	for (int i = architecture-1; i >= 0; --i)
+	{
+		printf("%d ", bits[i]);
+	}
+	printf("\n\n");
+	convert_int_to_binary(bits, 1, -1, architecture);
+	printf("mask -1 : 1 = %d = ", 1);
+	for (int i = architecture-1; i >= 0; --i)
+	{
+		printf("%d ", bits[i]);
+	}
+	printf("\n\n");
+	convert_int_to_binary(bits, 2, 1, architecture);
+	printf("mask 1 : 2 = %d = ", 2);
+	for (int i = architecture-1; i >= 0; --i)
+	{
+		printf("%d ", bits[i]);
+	}
+	printf("\n\n");
+	convert_int_to_binary(bits, 2, -1, architecture);
+	printf("mask -1 : 2 = %d = ", 2);
+	for (int i = architecture-1; i >= 0; --i)
+	{
+		printf("%d ", bits[i]);
+	}
+	printf("\n\n");
+	convert_int_to_binary(bits, 3, 1, architecture);
+	printf("mask 1 : 3 = %d = ", 3);
+	for (int i = architecture-1; i >= 0; --i)
+	{
+		printf("%d ", bits[i]);
+	}
+	printf("\n\n");
+	convert_int_to_binary(bits, 3, -1, architecture);
+	printf("mask -1 : 3 = %d = ", 3);
+	for (int i = architecture-1; i >= 0; --i)
+	{
+		printf("%d ", bits[i]);
+	}
+	printf("\n\n");
+	convert_int_to_binary(bits, 4, 1, architecture);
+	printf("mask 1 : 4 = %d = ", 4);
+	for (int i = architecture-1; i >= 0; --i)
+	{
+		printf("%d ", bits[i]);
+	}
+	printf("\n\n");
+	convert_int_to_binary(bits, 4, -1, architecture);
+	printf("mask -1 : 4 = %d = ", 4);
+	for (int i = architecture-1; i >= 0; --i)
+	{
+		printf("%d ", bits[i]);
+	}
+	printf("\n\n");
+	convert_int_to_binary(bits, 5, 1, architecture);
+	printf("mask 1 : 5 = %d = ", 5);
+	for (int i = architecture-1; i >= 0; --i)
+	{
+		printf("%d ", bits[i]);
+	}
+	printf("\n\n");
+	convert_int_to_binary(bits, 5, -1, architecture);
+	printf("mask -1 : 5 = %d = ", 5);
+	for (int i = architecture-1; i >= 0; --i)
+	{
+		printf("%d ", bits[i]);
+	}
+	printf("\n\n");
+	convert_int_to_binary(bits, 6, 1, architecture);
+	printf("mask 1 : 6 = %d = ", 6);
+	for (int i = architecture-1; i >= 0; --i)
+	{
+		printf("%d ", bits[i]);
+	}
+	printf("\n\n");
+	convert_int_to_binary(bits, 6, -1, architecture);
+	printf("mask -1 : 6 = %d = ", 6);
+	for (int i = architecture-1; i >= 0; --i)
+	{
+		printf("%d ", bits[i]);
+	}
+	printf("\n\n");
+	convert_int_to_binary(bits, 3, 1, architecture);
+	printf("mask 1 : 3 = %d = ", 3);
+	for (int i = architecture-1; i >= 0; --i)
+	{
+		printf("%d ", bits[i]);
+	}
+	printf("\n\n");
+	convert_int_to_binary(bits, 3, -1, architecture);
+	printf("mask -1 : 3 = %d = ", 3);
+	for (int i = architecture-1; i >= 0; --i)
+	{
+		printf("%d ", bits[i]);
+	}
+	printf("\n\n");
+	convert_int_to_binary(bits, 2147483647, 1, architecture);
+	printf("mask 1 : 2147483647 = %d = ", 2147483647);
+	for (int i = architecture-1; i >= 0; --i)
+	{
+		printf("%d ", bits[i]);
+	}
+	printf("\n\n");
+	convert_int_to_binary(bits, 2147483647, -1, architecture);
+	printf("mask -1 : 2147483647 = %d = ", 2147483647);
+	for (int i = architecture-1; i >= 0; --i)
+	{
+		printf("%d ", bits[i]);
+	}
+	printf("\n\n");
+	convert_int_to_binary(bits, 2147483648, 1, architecture);
+	printf("mask 1 : 2147483648 = %d = ", 2147483648);
+	for (int i = architecture-1; i >= 0; --i)
+	{
+		printf("%d ", bits[i]);
+	}
+	printf("\n\n");
+	convert_int_to_binary(bits, 2147483648, -1, architecture);
+	printf("mask -1 : 2147483648 = %d = ", 2147483648);
+	for (int i = architecture-1; i >= 0; --i)
+	{
+		printf("%d ", bits[i]);
+	}
+	printf("\n\n");
+	convert_int_to_binary(bits, 2147483649, 1, architecture);
+	printf("mask 1 : 2147483649 = %d = ", 2147483649);
+	for (int i = architecture-1; i >= 0; --i)
+	{
+		printf("%d ", bits[i]);
+	}
+	printf("\n\n");
+	convert_int_to_binary(bits, 2147483649, -1, architecture);
+	printf("mask -1 : 2147483649 = %d = ", 2147483649);
+	for (int i = architecture-1; i >= 0; --i)
+	{
+		printf("%d ", bits[i]);
+	}
+	printf("\n\n");
+	convert_int_to_binary(bits0, 4294967296, 1, 64);
+	printf("mask 1 : 4294967296 = %d = ", 4294967296);
+	for (int i = 64-1; i >=	 0; --i)
+	{
+		printf("%d ", bits0[i]);
+	}
+	printf("\n\n");
+	convert_int_to_binary(bits0, 4294967297, 1, 64);
+	printf("mask 1 : 4294967297 = %d = ", 4294967297);
+	for (int i = 64-1; i >=	 0; --i)
+	{
+		printf("%d ", bits0[i]);
+	}
+	printf("\n\n");
+	convert_int_to_binary(bits, 4294967296, -1, architecture);
+	printf("mask -1 : 4294967296 = %d = ", 4294967296);
+	for (int i = architecture-1; i >= 0; --i)
+	{
+		printf("%d ", bits[i]);
+	}
+	printf("\n\n");
 	free(bits);
-	free(result);
+	free(bits0);
+	architecture = 0;
 	return 0;
 }
